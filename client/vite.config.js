a80679384fca78c1a10c1d3024fdcdd652ab1c5b
RamNallapati <ramnallapati741@gmail.com>
2025-09-15 06:41:43 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          monaco: ['monaco-editor']
+        }
+      }
+    }
+  },
   server: {
     port: 3000,
     proxy: {
